7c0a18640e1f700abb6c9233cac4f7933aa97605
l3njo <plenjo15@gmail.com>
2019-07-06 09:30:36 +0300

    Initial commit
    Project setup.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace micro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
